2d66a32a1550a8999e089332ba24c333d937f976
David-Lucas17 <lucasehermilda@gmail.com>
2026-05-05 23:59:29 -0300

    redes neurais CNN

---
+++ deteccao_face_hog2.py
@@ -0,0 +1,17 @@
+import cv2
+import dlib
+
+subdetector = ["Olhar a frente", "Vista a esquerda", "Vista a direita", "A frente girando para a esquerda", "A frente girando para a direita"]
+
+imagem = cv2.imread("fotos/grupo.0.jpg")
+detector = dlib.get_frontal_face_detector()
+facesDetectadas, pontuacao, idx = detector.run(imagem)
+
+for i, d in enumerate(facesDetectadas):
+    print("Detecção: {}, pontuação: {}, Sub-detector: {}". format(d, pontuacao[i], subdetector(idx[i])))
+    e, t, d, b = (int(d.left()), int(d.top()), int(d.right()), int(d.bottom()))
+    cv2.rectangle(imagem, (e, t), (d, b), (0, 0, 255), 2)
+
+cv2.imshow("Detector hog", imagem)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
